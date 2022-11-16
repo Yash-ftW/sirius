8848a5b2c76ab013ca4e8fedf76a151b09fcb274
Fluffy <87795895+MrFluffy-ftW@users.noreply.github.com>
2022-11-16 19:53:15 +0545

    Login Page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Navbar, Container, Button, Offcanvas } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
+import { Link } from "react-router-dom";
 
 function Header() {
   const [show, setShow] = useState(false);
@@ -15,9 +16,34 @@ function Header() {
           </Button>
           <Offcanvas show={show} onHide={handleClose}>
             <Offcanvas.Header closeButton>
-              <Offcanvas.Title>Menu</Offcanvas.Title>
+              <Offcanvas.Title>
+                <h1>Menu</h1>
+              </Offcanvas.Title>
             </Offcanvas.Header>
-            <Offcanvas.Body>Links Here</Offcanvas.Body>
+            <Offcanvas.Body>
+              <ul class="list-group">
+                <Link to="/" className="link-no">
+                  <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Graph
+                  </li>
+                </Link>
+                <Link to="/" className="link-no">
+                  <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Schedule
+                  </li>
+                </Link>
+                <Link to="/" className="link-no">
+                  <li className="list-group-item d-flex justify-content-between align-items-center">
+                    Calendar
+                  </li>
+                </Link>
+                <Link to="/" className="link-no">
+                  <li class="list-group-item d-flex justify-content-between align-items-center">
+                    Details
+                  </li>
+                </Link>
+              </ul>
+            </Offcanvas.Body>
           </Offcanvas>
 
           <Navbar.Collapse className="justify-content-end">
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 function HomeScreen() {
+  const [user, setUser] = useState(0);
+
+  const handleUser = () => {
+    if (user === 1) {
+      setUser(0);
+    } else {
+      setUser(1);
+    }
+  };
   return (
     <div>
-      <h3>Home Screen</h3>
+      <button onClick={handleUser}>User Logged In status Switch</button>
+      {user === 1 ? (
+        <h1>User</h1>
+      ) : (
+        <Row className="py-3">
+          <Col>
+            <h2>Login To Continue</h2>
+            <Link to={"/login"}>Login</Link>
+          </Col>
+        </Row>
+      )}
     </div>
   );
 }
