588d0ff50381f0830debbbef5a1ea4d0e19d7820
Fluffy <87795895+MrFluffy-ftW@users.noreply.github.com>
2022-11-16 19:22:36 +0545

    Login Screen UI Added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import Footer from "./components/Footer";
 import HomeScreen from "./screens/HomeScreen";
-import { Container } from "react-bootstrap";
-import { HashRouter as Router, Route, Routes } from "react-router-dom";
+import LoginScreen from "./screens/LoginScreen";
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
         <Container>
           <Routes>
             <Route path="/" element={<HomeScreen />} exact />
+            <Route path="/login" element={<LoginScreen />} />
           </Routes>
         </Container>
       </main>
