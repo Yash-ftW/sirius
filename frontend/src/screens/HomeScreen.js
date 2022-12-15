ca8f13ffa68cabbf164879245a1fcd55adb1f2e6
Fluffy <87795895+MrFluffy-ftW@users.noreply.github.com>
2022-12-15 21:53:20 +0545

    Design 2

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 
-import Img1 from "../images/logo.png";
-import Img2 from "../images/logo.png";
+import Img1 from "../images/report.jpeg";
+import Img2 from "../images/analysis.jpg";
+import Img3 from "../images/recommend.jpg";
 
 function HomeScreen() {
   return (
@@ -11,7 +12,23 @@ function HomeScreen() {
         <h1>SIRUS</h1>
         <h3>Student Evaluation System</h3>
       </div>
-      <div className="features">
+      <section className="feat">
+        <h2>Features</h2>
+        <div className="line"></div>
+        <div className="features">
+          <div className="feature feature-1">
+            <h5>Report Prediction</h5>
+            <img src={Img1} alt="Feature1" />
+          </div>
+          <div className="feature feature-2">
+            <h5>Student Analysis</h5> <img src={Img2} alt="Feature2" />
+          </div>
+          <div className="feature feature-3">
+            <h5>Recommendation</h5> <img src={Img3} alt="Feature3" />
+          </div>
+        </div>
+      </section>
+      {/* <div className="features">
         <div className="feature feature-1">
           <h2>Report Prediction</h2>
         </div>
@@ -24,7 +41,7 @@ function HomeScreen() {
         <div className="feature feature-4">
           <img src={Img2} alt="Feature2" />
         </div>
-      </div>
+      </div> */}
     </Container>
   );
 }
